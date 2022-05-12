35919bcefc5a4910c56cc1db2bd01ffa594afdbe
RennerFox <matheusrennerdev@outlook.com>
2022-05-12 00:25:31 -0300

    Update files

---
+++ src/models/user.js
@@ -0,0 +1,38 @@
+const mongoose = require('../database');
+const bcrypt = require('bcryptjs');
+
+const UserSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+
+    email: {
+        type: String,
+        unique: true,
+        required: true,
+        lowercase: true,
+    },
+    
+    password: {
+        type: String,
+        required: true,
+        select: false
+    },
+
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+});
+
+UserSchema.pre('save', async function(next) {
+    const hash = await bcrypt.hash(this.password, 5);
+    this.password = hash;
+
+    next();
+});
+
+const User = mongoose.model('User', UserSchema);
+
+module.exports = User;
